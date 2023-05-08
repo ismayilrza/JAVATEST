ecdea5042dd8b0272018964b9a48a056bbe736e2
ismayilrza <irgbo@yahoo.com>
2023-05-08 15:42:26 -0400

    ss

---
+++ src/TestCGI.java
@@ -0,0 +1,143 @@
+import javax.sound.sampled.Line;
+import java.awt.*;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+import java.util.*;
+
+
+public class TestCGI {
+//
+//    Integer b = 2;
+//    int a = 1;
+//    a = b;
+//
+//
+//    public Integer getInt(){
+//
+//        return b;
+//    }
+//
+//
+//
+//}
+
+
+//public class Test1 {
+//
+//    TestCGI ab  = new TestCGI();
+//    int c = ab.getInt();
+
+
+//    public static void main(String[] args) throws IOException {
+//
+////        FileReader fr = new FileReader("/C:/Users/ismay/OneDrive/Desktop/testfile.txt");
+//       // String path = System.getProperty("user.dir")+"testCGI.txt";
+//        String path  = "resources/testCGI.txt";
+//        File   fr = new File(path);
+//        Scanner    sc = new Scanner(fr);
+//        FileWriter fw = null;
+//        BufferedWriter bw = null;
+//        PrintWriter pw = null;
+//        try{
+//            fw = new FileWriter(path, true);
+//            bw = new BufferedWriter(fw);
+//            pw = new PrintWriter(bw);
+//
+//            //   FileReader fr = new FileReader(path, Charset.forName("UTF8"));
+////       FileInputStream fr = new FileInputStream(path);
+////       fr.read();
+//            //File fr = new File(path);
+//            //read using Scanner
+//
+//            while(sc.hasNextLine()){
+//               pw.println(sc.nextLine());
+//            }
+//
+//
+//        }catch(IOException e){e.printStackTrace();}
+//        finally{
+//
+//            pw.flush();
+//        }
+//
+//       // File   fr2 = new File(path);
+//        Scanner    sc2 = new Scanner(fr);
+//        while(sc2.hasNextLine()){
+//            System.out.println(sc2.nextLine());
+//
+//        }
+
+
+    public static void main(String[] args) {
+//        String s = "asd";
+//        char[] letter = {'s','d'};
+        int a = 1234;
+    }
+
+    
+
+
+
+
+
+
+
+/*    public static void  sumOfDigits(int d){
+        int dd = d;
+        int temp = 0;
+        while(dd > 0){
+            temp = temp + dd%10;
+            dd = dd/10;
+            System.out.println(temp + "          "+dd);
+        }
+    }*/
+
+
+
+/*public static void reverseNumber(int n){
+        int nn = n;
+        int temp = 0;
+        int reversed = 0;
+
+        while(nn!=0){
+            temp= nn%10;
+            reversed = reversed*10+temp;
+            nn = nn/10;
+
+            System.out.println(temp +"      "+nn+"        "+reversed);
+        }
+        }*/
+
+
+
+
+/*    public static void printReverse(char[] letters, int size){
+        for (int i = letters.length-1; i >= 0 ; i--){
+            System.out.print(letters[i]);
+        }
+    }*/
+
+
+
+    /*    public static StringBuffer reverse2(String s){
+
+        StringBuffer r = new StringBuffer(s);
+
+       return  r.reverse();
+    }*/
+
+//    public static String reverse(String s){
+//        String r = "";
+//        for (int i = s.length() - 1; i >= 0; i--) {
+//            r += s.charAt(i);
+//        }
+//        return r;
+//    }
+
+
+
+
+
+}
+
